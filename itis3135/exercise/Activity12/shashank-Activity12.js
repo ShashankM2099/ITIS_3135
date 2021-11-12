d9d4dc5020c3905400bc87a8af50c58a26bc9664
Shashank <smondrat@uncc.edu>
2021-11-11 20:50:53 -0500

    Summary

---
+++ itis3135/exercise/Activity12/shashank-Activity12.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+    $("#slider").bxSlider({
+        auto: true,
+        minSlides: 1,
+        maxSlides: 1,
+        slideWidth: 300,
+        slideMargin: 22,
+        randomStart: true,
+        moveSlides: 1,
+        captions: true,
+        speed: 2950,
+        pagerType: 'short'
+    });
+});
